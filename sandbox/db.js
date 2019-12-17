1b64a90c65133c5a32acbce20b44490a5f94acd6
ekovalcov <e.kovalcov@gmail.com>
2019-12-17 23:11:18 +0300

    dbConnection

--- sandbox/db.js
+++ sandbox/db.js
@@ -1,20 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://ekovalcov:KfHan=4V!&dLDvEw)sWb}@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-// //   const collection = client.db("test").collection("devices");
-// const collection = db.collection("devices");
-//   console.log(collection.find({}))
-//   client.close();
-// });
+const url = "mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("sample_geospatial").collection("shipwrecks");
+  console.log(collection.find({}).toArray().then(result => {
+    console.log(result)
+  }))
 
-MongoClient.connect(uri, function(err, db) {
-
-    var cursor = client.db("test").collection("devices");
-
-    cursor.each(function(err, doc) {
-
-        console.log(doc);
-
-    });
-}); 
+});
