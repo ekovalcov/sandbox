9c24f535c2428c6fc1e7fe97b6006516b035f2c0
ekovalcov <e.kovalcov@gmail.com>
2019-12-19 22:55:19 +0300

    f

--- sandbox/db.js
+++ sandbox/db.js
@@ -1,11 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 client.connect(err => {
   const collection = client.db("sample_geospatial").collection("shipwrecks");
-  console.log(collection.find({}).toArray().then(result => {
+
+  console.log(collection.find({ watlev: 'always dry', feature_type: 'Wrecks - Visible', chart: 'US,US,graph,Chart 17405', }).toArray().then(result => {
     console.log(result)
+    client.close()
   }))
 
 });
