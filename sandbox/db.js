040d6a0d36ef1171b0bafc4e107419f054c0cd2e
ekovalcov <e.kovalcov@gmail.com>
2019-12-20 18:36:15 +0300

    f

--- sandbox/db.js
+++ sandbox/db.js
@@ -2,30 +2,22 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority';
 const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
-const get = () => {
+const get = (msg) => {
   return client.connect(err => {
     const collection = client.db("jokes").collection("jokes");
-    return collection.find({user:"1"}).toArray().then(result => {
-      send(result[0].joke)
-      return 'result';
-      
-      // client.close()
+    return collection.find({user:msg.text}).toArray().then(result => {
+      bot.sendMessage(msg.chat.id, result[0].joke)
     })
   
   });
 } 
-get()
 
 const TelegramBot = require('node-telegram-bot-api');
 const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
 
 const bot = new TelegramBot(token, {polling: true});
-
-const send = message => {
   bot.on('message', async (msg) => {
-
-    await bot.sendMessage(msg.chat.id, message);
-    console.log(message)
+    get(msg)
 });
-}
+
 
