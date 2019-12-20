ce5bb98cfdb40b0272320c7df16f93c376544857
ekovalcov <e.kovalcov@gmail.com>
2019-12-20 17:33:41 +0300

    asd

--- sandbox/db.js
+++ sandbox/db.js
@@ -2,13 +2,32 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const get = () => {
+  return client.connect(err => {
+    const collection = client.db("jokes").collection("jokes");
+    return collection.find({user:"1"}).toArray().then(result => {
+      send(result[0].joke)
+      return 'result';
+      
+      // client.close()
+    })
+  
+  });
+} 
+get()
 
-client.connect(err => {
-  const collection = client.db("sample_geospatial").collection("shipwrecks");
 
-  console.log(collection.find({ watlev: 'always dry', feature_type: 'Wrecks - Visible', chart: 'US,US,graph,Chart 17405', }).toArray().then(result => {
-    console.log(result)
-    client.close()
-  }))
 
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
+
+const bot = new TelegramBot(token, {polling: true});
+
+const send = message => {
+  bot.on('message', async (msg) => {
+
+    await bot.sendMessage(msg.chat.id, message);
+    console.log(message)
 });
+}
