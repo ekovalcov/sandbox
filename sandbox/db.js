a6fa042e4f7499385eb3ad87e715410c6cb25737
e.kovalcov <e.kovalcov@gmail.com>
2019-12-23 01:39:57 +0300

    separate db and tg functions

--- sandbox/db.js
+++ sandbox/db.js
@@ -1,63 +1,33 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://1ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority';
+const url = 'mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority';
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-var debug = require('debug')
+var debug = require('debug')('db')
+var dbName = 'jokes'
 
-
-debug('booting %o', 'name');
-client.connect(err => {
-  err? debug('catch error', err):
-  client
-    .db("jokes")
-    .collection("jokes")
-    .find({})
-    .toArray()
-    .then(result => {
-      console.log(result)
-      client.close()
+const findItem = (telegramMessage, bot) => {
+  console.log(bot)
+  client.connect(err => {
+    err? debug('error connection to db\n', err):
+    client.db(dbName).collection(dbName).find({}).toArray().then(result => {
+      bot.sendMessage(telegramMessage.chat.id, result[0].joke)
     })
-})
+    client.close()
+  })
+}
 
+const insertItem = telegramMessage => {
+  debug('establishing connect to', dbName)
+  client.connect(err => {
+    err? debug('error connection to db\n', err):
+    client.db(dbName).collection(dbName).insert(telegramMessage.text)
+    bot.sendMessage(telegramMessage.chat.id, 'Your joke has been added!')
+    client.close()
+  })
+}
 
-
-
-
-
-// const get = (msg) => {
-//   client.connect(err => {
-//     client
-//       .db("jokes")
-//       .collection("jokes")
-//       .find({user:msg.text})
-//       .toArray()
-//       .then(result => {
-//         bot.sendMessage(msg.chat.id, result[0].joke)
-//       })
-//   });
-// } 
-
-
-// const get = (msg) => {
-//   client.connect()
-
-//   client.connect(err => {
-//     client
-//       .db("jokes")
-//       .collection("jokes")
-//       .find({user:msg.text})
-//       .toArray()
-//       .then(result => {
-//         bot.sendMessage(msg.chat.id, result[0].joke)
-//       })
-//   });
-// }
-
-// const TelegramBot = require('node-telegram-bot-api');
-// const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
-
-// const bot = new TelegramBot(token, {polling: true});
-//   bot.on('message', (msg) => {
-//     get(msg)
-// });
+module.exports = {
+  findItem,
+  insertItem
+}
 
 
