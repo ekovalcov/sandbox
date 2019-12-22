7c1a17ef9e9be807c7e43721e8f11ff7cd6e2382
e.kovalcov <e.kovalcov@gmail.com>
2019-12-22 08:45:37 +0300

    f

--- sandbox/db.js
+++ sandbox/db.js
@@ -1,6 +1,27 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority';
-const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
+const url = 'mongodb+srv://1ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority';
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+var debug = require('debug')
+
+
+debug('booting %o', 'name');
+client.connect(err => {
+  err? debug('catch error', err):
+  client
+    .db("jokes")
+    .collection("jokes")
+    .find({})
+    .toArray()
+    .then(result => {
+      console.log(result)
+      client.close()
+    })
+})
+
+
+
+
+
 
 // const get = (msg) => {
 //   client.connect(err => {
@@ -16,27 +37,27 @@ const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: 
 // } 
 
 
-const get = (msg) => {
-  client.connect()
+// const get = (msg) => {
+//   client.connect()
 
-  client.connect(err => {
-    client
-      .db("jokes")
-      .collection("jokes")
-      .find({user:msg.text})
-      .toArray()
-      .then(result => {
-        bot.sendMessage(msg.chat.id, result[0].joke)
-      })
-  });
-}
+//   client.connect(err => {
+//     client
+//       .db("jokes")
+//       .collection("jokes")
+//       .find({user:msg.text})
+//       .toArray()
+//       .then(result => {
+//         bot.sendMessage(msg.chat.id, result[0].joke)
+//       })
+//   });
+// }
 
-const TelegramBot = require('node-telegram-bot-api');
-const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
+// const TelegramBot = require('node-telegram-bot-api');
+// const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
 
-const bot = new TelegramBot(token, {polling: true});
-  bot.on('message', (msg) => {
-    get(msg)
-});
+// const bot = new TelegramBot(token, {polling: true});
+//   bot.on('message', (msg) => {
+//     get(msg)
+// });
 
 
