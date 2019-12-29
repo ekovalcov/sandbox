9e5e715f07279e5b1ce9900d8401c18825e5aa0e
e.kovalcov <e.kovalcov@gmail.com>
2019-12-29 22:50:04 +0300

    move startup code to index.js

--- sandbox/index.js
+++ sandbox/index.js
@@ -2,13 +2,11 @@
 
 var MongoClient = require('mongodb').MongoClient;
 const TelegramBot = require('node-telegram-bot-api');
-const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
 var debug = require('debug')
-const { findItem } = require('./db')
-
 const { createObjectForIdSearch } = require('./helpers')
 
 const url = 'mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority';
+const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
 var dbName = 'jokes'
 
 
@@ -21,9 +19,9 @@ client.connect(err => {
     dbCollection = client.db(dbName).collection(dbName, () => {
         bot = new TelegramBot(token, { polling: true });
         bot.onText(/\/j (.+)/, (msg) => {
-            const jokeText = dbCollection.find().toArray(createObjectForIdSearch(msg)).then(result => {
-              debug('RESULT\n', result)
-              bot.sendMessage(msg.chat.id, JSON.stringify(jokeText))
+            dbCollection.find(createObjectForIdSearch(msg)).toArray().then(result => {
+                console.log(result)
+              bot.sendMessage(msg.chat.id, JSON.stringify(result[0].joke))
             })
         });
     })
--- sandbox/test/test.js
+++ sandbox/test/test.js
@@ -7,24 +7,24 @@ const {createObjectForIdSearch} = require('../helpers')
 const {testMsg} = require('./testData')
 
 
-describe('db', function() {
-    describe('findItem if key exsists', function() {
-      it('should return concrete object', async function() {
-        await assert.equal(await findItem(testMsg), {});
-      });
-    });
-});
-
-// describe('helpers.js', function() {
-//   describe('getMsgText', function() {
-//     it('из msg достается правильный текст команды', async function() {
-//       await assert.equal(getMsgText(testMsg), 999999);
+// describe('db', function() {
+//     describe('findItem if key exsists', function() {
+//       it('should return concrete object', async function() {
+//         await assert.equal(await findItem(testMsg), {});
+//       });
 //     });
-//   });
-
-//   describe('createObjectForIdSearch', function() {
-//     it('формируется корректный JSON для поиска шутки по id', async function() {
-//       await assert.deepStrictEqual(createObjectForIdSearch(testMsg), {user: "999999"});
-//     });
-//   });
 // });
+
+describe('helpers.js', function() {
+  describe('getMsgText', function() {
+    it('из msg достается правильный текст команды', async function() {
+      await assert.equal(getMsgText(testMsg), 999999);
+    });
+  });
+
+  describe('createObjectForIdSearch', function() {
+    it('формируется корректный JSON для поиска шутки по id', async function() {
+      await assert.deepStrictEqual(createObjectForIdSearch(testMsg), {user: "999999"});
+    });
+  });
+});
