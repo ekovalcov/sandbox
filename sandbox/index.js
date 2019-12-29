c977283afaf19c4220619dcfd63ce537253c36fd
e.kovalcov <e.kovalcov@gmail.com>
2019-12-30 01:21:15 +0300

    f

--- sandbox/index.js
+++ sandbox/index.js
@@ -2,12 +2,12 @@
 
 var MongoClient = require('mongodb').MongoClient;
 const TelegramBot = require('node-telegram-bot-api');
-var debug = require('debug')
-const { createObjectForIdSearch } = require('./helpers')
+var debug = require('debug');
+const { createObjectForIdSearch } = require('./helpers');
 
 const url = 'mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority';
 const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
-var dbName = 'jokes'
+var dbName = 'jokes';
 
 
 var client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,13 +21,12 @@ client.connect(err => {
         bot.onText(/\/j (.+)/, (msg) => {
             dbCollection.find(createObjectForIdSearch(msg)).toArray().then(result => {
                 result.forEach(elem => {
-                    bot.sendMessage(msg.chat.id, JSON.stringify(elem.joke))
-                })
-                
-            })
+                    bot.sendMessage(msg.chat.id, JSON.stringify(elem.joke));
+                });  
+            });
         });
-    })
-})
+    });
+});
 
 
 
