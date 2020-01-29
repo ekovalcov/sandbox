9192b7e3781f33044ed384e65f93d4944307a72e
ekovalcov <e.kovalcov@gmail.com>
2020-01-29 10:33:11 +0300

    refactorIndex

--- index.js
+++ index.js
@@ -4,32 +4,35 @@ var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 const TelegramBot = require('node-telegram-bot-api');
 const { createObjectForIdSearch } = require('./helpers');
+const {getJokesByAuthor, getConnection} = require('./db')
+const printTelegram = require('debug')('telegram')
 
-const url = 'mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net/test?retryWrites=true&w=majority';
 const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
-var dbName = 'jokes';
 
+const bot = new TelegramBot(token, { polling: true });
 
-var client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+var connect;
+(async () => {
+    connect = await getConnection()
+})()
 
-var dbCollection, bot;
+bot.onText(/\/j (.+)/, (telegramMessage) => {
+    printTelegram(telegramMessage)
+    const jokes = getJokesByAuthor("tester", connect)
+    jokes.forEach(elem => {
+        bot.sendMessage(telegramMessage.chat.id, elem.joke);
+    });  
+});
 
-var app = express();
-var port = process.env.PORT || 3002
-app.listen(port, function () {
-    client.connect(err => {
-        dbCollection = client.db(dbName).collection(dbName, () => {
-            bot = new TelegramBot(token, { polling: true });
-            bot.onText(/\/j (.+)/, (msg) => {
-                dbCollection.find(createObjectForIdSearch(msg)).toArray().then(result => {
-                    result.forEach(elem => {
-                        bot.sendMessage(msg.chat.id, elem.joke);
-                    });  
-                });
-            });
-        });
-    });
-  });
+// var port = process.env.PORT || 3002
+// app.listen(port, function () {
+//     client.connect(err => {
+//         dbCollection = client.db(dbName).collection(dbName, () => {
+           
+            
+//         });
+//     });
+// }); 
 
 
 
