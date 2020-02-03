673337bd8538a24db2891c371b8f27b22b1c36c5
e.kovalcov <e.kovalcov@gmail.com>
2020-02-03 17:38:30 +0300

    addPrettier

--- db.js
+++ db.js
@@ -1,34 +1,40 @@
-const MongoClient = require('mongodb').MongoClient;
-const printCollection = require('debug')('collection'),
-      printConnection = require('debug')('connection'), 
-      printSearchResult = require('debug')('search');
-const {toSearchByAuthorObject} = require('./helpers')
+const MongoClient = require("mongodb").MongoClient;
+const printCollection = require("debug")("collection"),
+  printConnection = require("debug")("connection"),
+  printSearchResult = require("debug")("search");
+const { toSearchByAuthorObject } = require("./helpers");
 
-const url = 'mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net';
-const dbName = 'jokes'; 
- 
-const getConnection = async () => await MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true }).connect()
-const extractCollection = connection => connection.db(dbName).collection(dbName);
+const url =
+  "mongodb+srv://ekovalcov:KfHan%3D4V%21%26dLDvEw%29sWb%7D@cluster0-o2rqp.mongodb.net";
+const dbName = "jokes";
+
+const getConnection = async () =>
+  await MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).connect();
+const extractCollection = connection =>
+  connection.db(dbName).collection(dbName);
 
 const getJokesByAuthor = async (telegramMessage, connect) => {
-  printConnection(connect)
-  const collection = extractCollection(connect)
-  printCollection(collection)
-  const res = await collection.find(toSearchByAuthorObject(telegramMessage)).toArray()
-  printSearchResult(res)
-  return res
-} 
+  printConnection(connect);
+  const collection = extractCollection(connect);
+  printCollection(collection);
+  const res = await collection
+    .find(toSearchByAuthorObject(telegramMessage))
+    .toArray();
+  printSearchResult(res);
+  return res;
+};
 
 const insertJoke = async (telegramMessage, connect) => {
-  printConnection(connection)
-  const collection = extractCollection(connect)
-  printCollection(collection)
-  return await collection.insert(joke)
-}
+  printConnection(connection);
+  const collection = extractCollection(connect);
+  printCollection(collection);
+  return await collection.insert(joke);
+};
 
 module.exports = {
   getConnection,
   getJokesByAuthor
-}
-
-
+};
--- index.js
+++ index.js
@@ -1,51 +1,45 @@
-'use strict'
+"use strict";
 
-const TelegramBot = require('node-telegram-bot-api');
-const {getJokesByAuthor, getConnection} = require('./db')
-const printTelegramMessage = require('debug')('telegram')
-var express = require('express');
-var request = require('request');
+const TelegramBot = require("node-telegram-bot-api");
+const { getJokesByAuthor, getConnection } = require("./db");
+const printTelegramMessage = require("debug")("telegram");
+var express = require("express");
+var request = require("request");
 
-
-const token = '980381562:AAHJ2Xra1x7JuKCM2y1z__S8KJ3m3e_R8P0';
+const token = "980381562:AAHJ2Xra1x7JuKCM2y1z__S8KJ3m3e_R8P0";
 
 const bot = new TelegramBot(token, { polling: true });
 var connect;
 
-
 var app = express();
-var port = process.env.PORT || 3002 
+var port = process.env.PORT || 3002;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, function () { 
-    
-    (async () => {
-        connect = await getConnection()
-    })()
+app.listen(port, function() {
+  (async () => {
+    connect = await getConnection();
+  })();
 
-    bot.onText(/\/j (.+)/, async telegramMessage => {
-        printTelegramMessage(telegramMessage)
-        const jokes = await getJokesByAuthor(telegramMessage, connect)
-        if (jokes.length === 0) {
-            bot.sendMessage(telegramMessage.chat.id, 'Автор не умеет шутить (шуток не найдено)');
-        }
-        jokes.forEach(elem => {
-            bot.sendMessage(telegramMessage.chat.id, elem.joke);
-        });  
+  bot.onText(/\/j (.+)/, async telegramMessage => {
+    printTelegramMessage(telegramMessage);
+    const jokes = await getJokesByAuthor(telegramMessage, connect);
+    if (jokes.length === 0) {
+      bot.sendMessage(
+        telegramMessage.chat.id,
+        "Автор не умеет шутить (шуток не найдено)"
+      );
+    }
+    jokes.forEach(elem => {
+      bot.sendMessage(telegramMessage.chat.id, elem.joke);
     });
+  });
 
-    bot.onText(/\/help (.+)/, async telegramMessage => {
-        printTelegramMessage(telegramMessage)
-        bot.sendMessage(telegramMessage.chat.id, 'Привет, \nсписок доступных команд:\n/j - поиск всех шуток по имени автора: /j Влад');
-    });
-}); 
-
-
-
-
-
-
-
-
-
+  bot.onText(/\/help (.+)/, async telegramMessage => {
+    printTelegramMessage(telegramMessage);
+    bot.sendMessage(
+      telegramMessage.chat.id,
+      "Привет, \nсписок доступных команд:\n/j - поиск всех шуток по имени автора: /j Влад"
+    );
+  });
+});
