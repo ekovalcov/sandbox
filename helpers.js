b3ab8f0e8646c5a564d5c68cc596a7508aed1835
e.kovalcov <e.kovalcov@gmail.com>
2020-02-03 20:08:02 +0300

    checkFormat

--- helpers.js
+++ helpers.js
@@ -1,14 +1,13 @@
-'use strict'
+"use strict";
 
-const getMsgText = msg => msg.text.split(' ')[1]
+const getMsgText = msg => msg.text.split(" ")[1];
 
 const toSearchByAuthorObject = function(telegramMessage) {
-    const messageText = getMsgText(telegramMessage)
-    return {author: messageText}
-}
+  const messageText = getMsgText(telegramMessage);
+  return { author: messageText };
+};
 
- module.exports = {
-     getMsgText,
-     toSearchByAuthorObject
- }
-
+module.exports = {
+  getMsgText,
+  toSearchByAuthorObject
+};
--- telegram.js
+++ telegram.js
@@ -1,13 +1,13 @@
-'use strict'
+"use strict";
 
-const TelegramBot = require('node-telegram-bot-api');
-const token = '980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo';
+const TelegramBot = require("node-telegram-bot-api");
+const token = "980381562:AAH2Fz8UjC5w-eut-FoGaM8bywUIBLF1Pmo";
 const bot = new TelegramBot(token, { polling: true });
-var debug = require('debug')
-const {findItem} = require('./db')
+var debug = require("debug");
+const { findItem } = require("./db");
 
-bot.onText(/\/j (.+)/,  (msg) => {
-    findItem(msg, bot)
+bot.onText(/\/j (.+)/, msg => {
+  findItem(msg, bot);
 });
 
 // bot.onText(/\/add (.+)/, (msg, match) => {
