ac8883b76bc1a51e92c2223fe8d42b79d045a7c3
ekovalcov <e.kovalcov@gmail.com>
2020-02-20 03:49:58 +0300

    f

--- telegram.js
+++ telegram.js
@@ -1,14 +1,14 @@
-"use strict";
+// "use strict";
 
-const TelegramBot = require("node-telegram-bot-api");
-const token = process.env.TELEGRAM_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
-const { findItem } = require("./db");
+// const TelegramBot = require("node-telegram-bot-api");
+// const token = process.env.TELEGRAM_TOKEN;
+// const bot = new TelegramBot(token, { polling: true });
+// const { findItem } = require("./db");
 
-bot.onText(/\/j (.+)/, msg => {
-  findItem(msg, bot);
-});
-
-// bot.onText(/\/add (.+)/, (msg, match) => {
-//     bot.sendMessage(msg.chat.id, 'added');
+// bot.onText(/\/j (.+)/, msg => {
+//   findItem(msg, bot);
 // });
+
+// // bot.onText(/\/add (.+)/, (msg, match) => {
+// //     bot.sendMessage(msg.chat.id, 'added');
+// // });
